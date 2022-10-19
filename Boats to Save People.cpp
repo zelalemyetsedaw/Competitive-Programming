c497e86770af0df42f73b1291475b9a34d5788ae
zelalemyetsedaw <zy94988@gmail.com>
2022-10-19 18:34:11 +0100

    Create Boats to Save People.cpp

---
+++ Boats to Save People.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int n = people.size();
+        int left = 0;
+        int right = n-1;
+        int count = 0;
+        sort(people.begin(),people.end());
+        while(left<=right)
+        {
+            if(people[left]+people[right]<= limit)
+            {
+                left++;
+               
+            }
+           
+            right--;
+            count++;
+        }
+        return count;
+    }
+};

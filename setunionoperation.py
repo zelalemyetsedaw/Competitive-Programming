5e11cfa894214b57c18bc47b3657ccc8bf79d8df
zelalemyetsedaw <zy94988@gmail.com>
2022-12-26 15:14:33 +0000

    Update setunionoperation.py

--- setunionoperation.py
+++ setunionoperation.py
@@ -1,7 +1,7 @@
 # Enter your code here. Read input from STDIN. Print output to STDOUT
 
-en = int(input())
+numOfEnglish = int(input())
 english = set(input().split())
-ef = int(input())
+numOfFrench = int(input())
 french = set(input().split())
 print(len(english.union(french)))

bef8a2fed7deaf211ca5efb22f8f9f4faf2d9993
zelalemyetsedaw <zy94988@gmail.com>
2023-01-17 15:48:42 +0000

    Time: 591 ms (10.42%), Space: 14.4 MB (84.60%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -5,10 +5,14 @@ class Solution:
         for i in range(n):
             d[heights[i]] = names[i]
         for i in range(n):
-            for j in range(n-1):
-                if heights[i]>heights[j]:
-                    heights[i],heights[j] = heights[j],heights[i]
+            mini = i
+            for j in range(i+1,n):
+                if heights[mini]<heights[j]:
+                    mini = j
+            if i!=mini:
+                heights[i],heights[mini] = heights[mini],heights[i]
         
+        output = []
         for i in range(n):
-            names[i] =d[heights[i]]
-        return names
+            output.append(d[heights[i]])
+        return output

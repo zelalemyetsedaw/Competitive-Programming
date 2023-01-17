9a5ac815f45712a12ecd423b2a74425e4349ebc6
zelalemyetsedaw <zy94988@gmail.com>
2023-01-17 15:52:15 +0000

    Time: 578 ms (10.92%), Space: 14.3 MB (99.41%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.py
+++ 2418-sort-the-people/2418-sort-the-people.py
@@ -1,18 +1,16 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        d = defaultdict()
+       
         n = len(names)
-        for i in range(n):
-            d[heights[i]] = names[i]
+        
         for i in range(n):
             mini = i
             for j in range(i+1,n):
                 if heights[mini]<heights[j]:
                     mini = j
             if i!=mini:
-                heights[i],heights[mini] = heights[mini],heights[i]
+                heights[i],heights[mini]= heights[mini],heights[i]
+                names[i],names[mini] = names[mini],names[i]
         
-        output = []
-        for i in range(n):
-            output.append(d[heights[i]])
-        return output
+        
+        return names

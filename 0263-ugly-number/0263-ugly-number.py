66add89cf230aeced634de30c93865cd64c149e0
zelalemyetsedaw <zy94988@gmail.com>
2023-02-06 17:07:22 +0000

    Time: 35 ms (67.39%), Space: 13.9 MB (50.00%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.py
@@ -0,0 +1,18 @@
+
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n == 1 and n==2:
+            return True
+        while n>0:
+            if n==1:
+                return True
+            elif n%2 == 0:
+                n/=2
+            elif n%3 == 0:
+                n/=3
+            elif n%5 == 0:
+                n/=5
+            else:
+                return False
+            
+      

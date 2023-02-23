63a8401b87289ae7a8507d4c3a34f7609e6de495
zelalemyetsedaw <zy94988@gmail.com>
2023-02-23 12:48:08 +0000

    Time: 193 ms (17.62%), Space: 14.9 MB (7.96%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -1,28 +1,14 @@
 class Solution(object):
     def findAnagrams(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: List[int]
-        """
-        dicts = defaultdict(int)
-        dictp = defaultdict(int)
-        for i in range(len(p)):
-            dictp[p[i]] += 1
-        
-        end = 0
-        start = 0
         result = []
-        while end<len(s):
-            dicts[s[end]] += 1
-            if end-start + 1 == len(p):
-                if dicts == dictp:
-                    result.append(start)
-                dicts[s[start]] -= 1
-                if(dicts[s[start]]==0):
-                    dicts.pop(s[start])
-                start += 1
-            end += 1
-            
-        return result           
+        target_counts = collections.Counter(p)
+        window_counts = collections.Counter(s[:len(p) - 1])
+        for i in range(len(p) - 1, len(s)):
+            window_counts[s[i]] += 1
+            if window_counts == target_counts:
+                 result.append(i - len(p) + 1)
+            window_counts[s[i - len(p) + 1]] -= 1
+            if window_counts[s[i - len(p) + 1]] == 0:
+                   del window_counts[s[i - len(p) + 1]]
+        return result          
         

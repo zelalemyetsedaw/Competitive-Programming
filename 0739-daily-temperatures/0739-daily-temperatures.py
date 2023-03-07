453d37b513d3efc4a4fd68766550f6da775daaeb
zelalemyetsedaw <zy94988@gmail.com>
2023-03-07 14:55:03 +0000

    Time: 1565 ms (18.31%), Space: 32.1 MB (6.01%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -1,6 +1,6 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        array = [[p,i] for i,p in enumerate(temperatures)]
+        array = [(p,i) for i,p in enumerate(temperatures)]
         
         stack = []  # monotonic decreasing order stack
         result = [0] * len(temperatures)

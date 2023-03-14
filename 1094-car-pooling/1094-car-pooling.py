68ffb68c1f3e3c409042771915a869ffa82ac6c0
zelalemyetsedaw <zy94988@gmail.com>
2023-03-14 07:05:01 +0000

    Time: 68 ms (91.39%), Space: 14.4 MB (78.65%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,19 +1,17 @@
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
-        lists = []
-        for n, start, end in trips:
-            lists.append((start, n))
-            lists.append((end, -n))
+       
+        
+        array = [0] * 1002
+        for num,fromm,to in trips:
+            array[fromm] += num
+            array[to ] -= num
             
-        
-        lists.sort()
-        
-        passanger = 0
-        flag = True
-        for item in lists:
-            passanger += item[1]
-            if passanger > capacity:
-                flag = False
-                
-        return flag
+        for i in range(1002):
+            array[i] += array[i-1]
+            
+        for i in array:
+            if i>capacity:
+                return False
+        return True
             

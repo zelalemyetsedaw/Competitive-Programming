613ac0dd6d89e8012ff3c07764bce74cceba7709
zelalemyetsedaw <zy94988@gmail.com>
2023-03-28 18:10:25 +0300

    Time: 64 ms (32.32%), Space: 13.9 MB (95.52%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -1,13 +1,39 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
-        rptr, wptr = 0, 0
-        while rptr < len(chars):
-            ch, f = chars[rptr], 0
-            while rptr < len(chars) and chars[rptr] == ch:
-                rptr, f = rptr+1, f+1
-            chars[wptr], wptr = ch, wptr + 1
-            if f > 1:
-                for c in str(f):
-                    chars[wptr], wptr = c, wptr + 1
-        return wptr
+        
+        left = 0
+        right = 1
+        count = 1
+        chars.append(" ")
+        
+        while right < len(chars):
+            if chars[right] == chars[right-1]:
+                count += 1
+            else:
+                if count == 1:
+                    chars[left] = chars[right-1]
+                    count = 1
+                elif count > 1 and count < 10:
+                    chars[left] = chars[right-1]
+                    left = left+1
+                    
+                    chars[left] = str(count)
+                    count = 1
+                else:
+                    chars[left] = chars[right-1]
+                    count = str(count)
+                    for i in range(len(count)):
+                        left += 1
+                        chars[left] = count[i]
+                        
+                        
+                    count = 1
+                left += 1
+            right += 1
+                
+        return left
+                
+        
+                    
+            
             

5f78c7a96b254f73d0b4065385046f75da84dcea
zelalemyetsedaw <zy94988@gmail.com>
2023-04-03 12:02:38 +0300

    Time: 6568 ms (10.38%), Space: 21.5 MB (84.88%) - LeetHub

---
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findSubsequences(self, nums: List[int]) -> List[List[int]]:
+        
+        def combination(index,arr,temp,n,answer):
+            if index == n :
+                if len(temp)>=2 and (not answer or  temp not in answer):
+                    answer.append(temp.copy())
+                return answer
+
+            if not temp or temp[-1] <= arr[index]:
+                temp.append(arr[index])
+                answer = combination(index+1,arr,temp,n,answer)
+
+                temp.pop()
+            answer = combination(index+1,arr,temp,n,answer)
+            return answer
+
+        
+        temp = []
+        n = len(nums)
+        answer = []
+
+        return list(combination(0,nums,temp,n,answer))
+
+        
+            

efd5fcc8c67ce1bcb24f8b8e43d887a24d24b937
zelalemyetsedaw <zy94988@gmail.com>
2023-04-27 19:44:23 +0300

    Time: 55 ms (40.48%), Space: 18.9 MB (7.72%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -6,30 +6,27 @@
 #         self.right = right
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
-        averages = [root.val/1]
+        averages = []
       
         queue =deque([root])
             
         while queue:
             l = len(queue)
-            node = queue.popleft()
+            
             summ,count = 0,0
             for i in range(l):
+                node = queue.popleft()
+                summ += node.val
                 if node.left:
                     queue.append(node.left)
-                    summ += node.left.val
-                    count += 1
+                    
+                    
                 if node.right:
                     queue.append(node.right)
-                    summ += node.right.val
-                    count += 1
-                if i==(l-1) and count:
                     
-                    averages.append(summ/count)
-                elif queue:
-                    node = queue.popleft()
+            
+            averages.append(summ/l)
+               
                     
                     
-                    
-        
         return averages

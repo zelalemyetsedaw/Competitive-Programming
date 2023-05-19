eaa56b7807bb2ce3fa1c69d49556d7749bcb4a2f
zelalemyetsedaw <zy94988@gmail.com>
2023-05-19 11:28:06 +0300

    Time: 79 ms (15.97%), Space: 17 MB (7.85%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.py
@@ -0,0 +1,55 @@
+class UnionFind:
+    def __init__(self, edges):
+        self.parent = defaultdict(int)
+        self.rank = defaultdict(int)
+        for item in edges:
+            self.parent[item[0]] = item[0]
+            self.parent[item[1]] = item[1]
+            self.rank[item[0]]= 0
+            self.rank[item[1]] = 0
+        
+        
+    def find(self, member):
+        self.root = member
+        while self.root != self.parent[self.root]:
+            self.root = self.parent[self.root]
+
+        while member != self.root:
+            parent = self.parent[member]
+            self.parent[member] = self.root
+            member = parent
+
+        return self.root
+		
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.parent[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.parent[rootX] = rootY
+            else:
+                self.parent[rootY] = rootX
+                self.rank[rootX] += 1
+        
+                
+
+    def connected(self, x, y):
+        return self.parent[x] == self.parent[y]
+    def check(self,x,y):
+        if self.find(x) == self.find(y):
+            return True
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        uf = UnionFind(edges)
+        for item in edges:
+            if  uf.check(item[0],item[1]):
+                return item
+            uf.union(item[0],item[1])
+            
+        
+            
+        
+        

acb5e4b07506370bc3bb0e04db9e000c9b763a18
zelalemyetsedaw <zy94988@gmail.com>
2023-05-19 11:02:19 +0300

    Time: 203 ms (33.01%), Space: 16.7 MB (32.73%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,37 +1,45 @@
 class Solution:
+    
+    class UnionFind:
+        def __init__(self, n: int):
+            self.parent = [i for i in range(n)]
+            self.treeSize = [1 for _ in range(n)]
+            self.count = n
+        
+        def union(self, a: int, b: int):
+            a = self._find(a)
+            b = self._find(b)
+            if a == b:
+                return
+            if self.treeSize[a] < self.treeSize[b]:
+                self.parent[a] = b
+                self.treeSize[b] += self.treeSize[a]
+            else:
+                self.parent[b] = a
+                self.treeSize[a] += self.treeSize[b]
+            self.count -= 1
+    
+        def _find(self, x: int) -> int:
+            while self.parent[x] != x:
+                self.parent[x] = self.parent[self.parent[x]]
+                x = self.parent[x]
+            return x
+			
 
+        def isConnected(self, a: int, b: int) -> bool:
+            a = self._find(a)
+            b = self._find(b)
+            return a == b
+    
+    
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        graph = defaultdict(list)
-        x = 202
-        for i in range(len(isConnected)):
-            for j in range(len(isConnected)):
-                if isConnected[i][j] == 1:
-                    
-                    graph[i].append(j)
-                    graph[j].append(i)
-                    
-        components = {}
-        
-        for city in graph:
-            components[city] = set([city])
-        
-        
-        for city in graph:
-            for neighbor in graph[city]:
-                if components[city] is not components[neighbor]:
-                    components[city] |= components[neighbor]
-                    
-                    for c in components[neighbor]:
-                        components[c] = components[city] # set references
-
-       
-        answer = set()
-        for i in components.values():
-            answer.add(tuple(i))
-                
-        return len(answer)
-            
-                
-        
-
-            
+        if not isConnected or len(isConnected) == 0 or len(isConnected[0]) == 0:
+            return 0
+        lenY, lenX = len(isConnected), len(isConnected[0])
+        uf = self.UnionFind(lenY)
+        cities = lenY
+        for y in range(lenY):
+            for x in range(y + 1, lenX):
+                if isConnected[y][x] == 1:
+                    uf.union(y, x)
+        return uf.count

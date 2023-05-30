56c6b07a50cfefacfb979856eddd64591ca433ef
zelalemyetsedaw <zy94988@gmail.com>
2023-05-30 18:36:42 +0300

    Time: 824 ms (43.76%), Space: 19.9 MB (54.92%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -0,0 +1,28 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        store5 = []
+        store10 = []
+        store20 = []
+        
+        for i in bills:
+            if i == 5:
+                store5.append(5)
+            elif i == 10:
+                if not store5:
+                    return False
+                else:
+                    store5.pop()
+                    store10.append(10)
+            else:
+                if store5 and store10:
+                    store5.pop()
+                    store10.pop()
+                    store20.append(20)
+                elif len(store5) >= 3:
+                    store5.pop()
+                    store5.pop()
+                    store5.pop()
+                else:
+                    return False
+        return True
+                    

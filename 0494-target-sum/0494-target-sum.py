852c812b8d60f60462d6341186443bae1a4519db
zelalemyetsedaw <zy94988@gmail.com>
2023-07-19 09:14:00 +0300

    Time: 360 ms (47.40%), Space: 37.8 MB (23.77%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,22 +1,29 @@
 class Solution:
-    def findTargetSumWays(self, nums, S):
-        index = len(nums) - 1
-        curr_sum = 0
-        self.memo = {}
-        return self.dp(nums, S, index, curr_sum)
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+                
+        memo = {}
         
-    def dp(self, nums, target, index, curr_sum):
-        if (index, curr_sum) in self.memo:
-            return self.memo[(index, curr_sum)]
-        
-        if index < 0 and curr_sum == target:
-            return 1
-        if index < 0:
-            return 0 
-        
-        positive = self.dp(nums, target, index-1, curr_sum + nums[index])
-        negative = self.dp(nums, target, index-1, curr_sum + -nums[index])
-        
-        self.memo[(index, curr_sum)] = positive + negative
-        return self.memo[(index, curr_sum)]
-        
+        def recursion(cursum,index):
+       
+            
+            if index == len(nums):
+                if cursum == target:
+                    return 1
+                return 0
+            
+            if (index,cursum) in memo:
+                return memo[(index,cursum)]
+            
+            
+            
+            
+            memo[(index,cursum)] = recursion(cursum + nums[index],index + 1) +                                                  recursion(cursum - nums[index] ,index+1)
+            
+            
+            
+            
+            return memo[(index,cursum)]
+            
+       
+        return recursion(0,0)
+             

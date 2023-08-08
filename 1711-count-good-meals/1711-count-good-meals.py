37fbb49fbd0dfa010305dda598491e8ea97ff83f
zelalemyetsedaw <zy94988@gmail.com>
2023-08-08 22:48:16 +0300

    Time: 1501 ms (64.93%), Space: 24 MB (94.79%) - LeetHub

---
+++ 1711-count-good-meals/1711-count-good-meals.py
@@ -0,0 +1,26 @@
+class Solution:
+    def countPairs(self, deliciousness: List[int]) -> int:
+        sets = set()
+        
+        for i in range(22):
+            sets.add(2 ** i)
+            
+        d = defaultdict(int)
+        visited = defaultdict(int)
+        
+        for item in deliciousness:
+            d[item] += 1
+        count = 0  
+        
+        for item in sets:
+            for i in deliciousness:
+                if (item-i) in d and (item-i) != i:
+                    count += d[item-i]
+                    
+                elif (item-i) in d and (item-i) == i:
+                    count += d[item-i] - 1 
+                    visited[item-i] += 1
+                    
+        return (count//2) % (10**9 + 7)
+        
+        

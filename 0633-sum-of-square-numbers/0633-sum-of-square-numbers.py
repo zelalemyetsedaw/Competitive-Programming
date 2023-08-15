076569a88cdd23255c6efa89154b1bf63f17ae39
zelalemyetsedaw <zy94988@gmail.com>
2023-08-15 09:54:58 +0300

    Time: 314 ms (34.56%), Space: 16.4 MB (37.06%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.py
@@ -1,12 +1,15 @@
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
         
-        i = 1
-        sets = set([0])
-        while i**2 <= 2**31 -1:
-            sets.add(i**2)
-            i += 1
-        for i in sets:
-            if (c - i) in sets:
+        left = 0
+        right = int(sqrt(c))
+        
+        while left <= right:
+            sum = left ** 2 + right ** 2
+            if sum == c:
                 return True
+            elif sum < c:
+                left += 1
+            else:
+                right -= 1
         return False

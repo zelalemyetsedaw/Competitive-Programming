94c550b4a691ed1700663cf6d39f69be0e3dd318
zelalemyetsedaw <zy94988@gmail.com>
2023-08-15 10:48:38 +0300

    Time: 45 ms (83.43%), Space: 16.2 MB (93.78%) - LeetHub

--- 0763-partition-labels/0763-partition-labels.py
+++ 0763-partition-labels/0763-partition-labels.py
@@ -1,15 +1,27 @@
 class Solution:
-    def partitionLabels(self, S: str) -> List[int]:
-        rightmost = {c:i for i, c in enumerate(S)}
-        left, right = 0, 0
-
-        result = []
-        for i, letter in enumerate(S):
-
-            right = max(right,rightmost[letter])
-
-            if i == right:
-                result += [right-left + 1]
-                left = i+1
-
-        return result
+    def partitionLabels(self, s: str) -> List[int]:
+        
+        lastindex = defaultdict(int)
+        for i in range(len(s)):
+            lastindex[s[i]] = i
+            
+        initialindex = 0
+        left = 0
+        right = 0
+        ans = []
+        while left < len(s):
+            
+            if lastindex[s[left]] > right:
+                right = lastindex[s[left]]
+                
+            if left == right:
+                ans.append(right - initialindex + 1)
+                initialindex = left + 1
+            left += 1
+        
+        return ans
+                
+                
+            
+            
+            

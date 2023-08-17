135f9118df7981f6272c40403dad52dc43f5b746
zelalemyetsedaw <zy94988@gmail.com>
2023-08-17 08:01:12 +0300

    Time: 1154 ms (82.46%), Space: 41.3 MB (87.72%) - LeetHub

--- 2381-shifting-letters-ii/2381-shifting-letters-ii.py
+++ 2381-shifting-letters-ii/2381-shifting-letters-ii.py
@@ -1,22 +1,25 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
-        array = [0] * (len(s) + 1)
-        for a,b,c in shifts:
-            if c == 0:
-                array[a] += -1
-                array[b+1] += 1
-            elif c==1:
-                array[a] += 1
-                array[b+1] -= 1
+        
+        prefixsum = [0] * (len(s) + 1)
+        
+        for start,end,direction in shifts:
+            if direction == 0:
+                prefixsum[start] += -1
+                prefixsum[end+1] += 1
+            else:
+                prefixsum[start] += 1
+                prefixsum[end+1] += -1
                 
-        for i in range(1,len(array)):
-            array[i] += array[i-1]
-        
-        ans = list(s)
-        
-       
-        for i in range(len(s)):
-            ans[i] = chr(((ord(ans[i])- 97 + array[i]) % 26)+97)
-        
-        return "".join(ans)
             
+        for i in range(1,len(s)+1):
+            prefixsum[i] += prefixsum[i-1]
+        
+        
+        answer = ""
+        for i in range(len(s)):
+            answer += chr((((ord(s[i]) + prefixsum[i])-97) %26) + 97)
+        
+        return answer
+        
+        

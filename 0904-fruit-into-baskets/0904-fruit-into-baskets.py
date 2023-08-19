1c31579a1110b2b17b98903623aa6c0f7b612a31
zelalemyetsedaw <zy94988@gmail.com>
2023-08-19 11:41:04 +0300

    Time: 800 ms (71.08%), Space: 22.6 MB (15.38%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.py
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.py
@@ -1,22 +1,18 @@
 class Solution:
     def totalFruit(self, fruits: List[int]) -> int:
-        counts2 = defaultdict(int)
+        
         left = 0
-        right = 0
-        count = 0
-        
+        dicts = defaultdict(int)
+        maxlen = 0
         for right in range(len(fruits)):
-            counts2[fruits[right]] += 1
-            while len(counts2) > 2:
-                counts2[fruits[left]] -= 1
-                if counts2[fruits[left]] == 0:
-                    counts2.pop(fruits[left])
+            dicts[fruits[right]] += 1
+            while len(dicts) > 2:
+                dicts[fruits[left]] -= 1
+                if dicts[fruits[left]] == 0:
+                    dicts.pop(fruits[left])
                 left += 1
-            count = max(count,right-left+1)
+                
+            maxlen = max(maxlen,right-left + 1)
             
-            
-        return count
-        
-            
-            
+        return maxlen
             

527a4bd0a1d8ae90a852c58e28486a9aff87f7a3
zelalemyetsedaw <zy94988@gmail.com>
2023-08-23 21:10:46 +0300

    Time: 5237 ms (5.60%), Space: 16.3 MB (63.83%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.py
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -1,18 +1,18 @@
 class Solution:
-    def PredictTheWinner(self, nums: List[int]) -> bool:
-        def getscore(left, right, turn):
-            if left == right:
-                return nums[left] * turn
-            
-            getleft = nums[left]*turn + getscore(left+1, right, -turn)
-            getright = nums[right]*turn + getscore(left, right-1, -turn)
-            
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        
+        def helper(left,right,score1,score2,turn):
+            if left > right:
+                return score1 >= score2
             if turn == 1:
-                return max(getleft, getright)
-            elif turn == -1:
-                return min(getleft, getright)
+                leftway = helper(left+1,right,score1+nums[left],score2,0)
+                rightway = helper(left,right-1,score1 + nums[right],score2,0)
+                
+                return leftway or rightway
+            else:
+                leftway = helper(left+1,right,score1,score2 + nums[left],1)
+                rightway = helper(left,right-1,score1,score2 + nums[right],1)
+                
+                return leftway and rightway
             
-        if getscore(0, len(nums)-1, 1) >= 0:
-            return True
-        else:
-            return False
+        return helper(0,len(nums)-1,0,0,1)

2045cb6bc41fb1c443c51055bdf85ba33baa796c
zelalemyetsedaw <zy94988@gmail.com>
2023-08-23 11:17:23 +0300

    Time: 38 ms (87.37%), Space: 16.3 MB (39.57%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,26 +1,26 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
         
-        def pascal(n):
-            if n == 1:
-                return [[1]]
-            array = pascal(n-1)
-            array2 = [0] + array[-1] + [0]
-            row = []
-            for i in range(len(array2) - 1):
-                row.append(array2[i] + array2[i+1])
-            array.append(row)
-            return array
-        return pascal(numRows)
+        # def pascal(n):
+        #     if n == 1:
+        #         return [[1]]
+        #     array = pascal(n-1)
+        #     array2 = [0] + array[-1] + [0]
+        #     row = []
+        #     for i in range(len(array2) - 1):
+        #         row.append(array2[i] + array2[i+1])
+        #     array.append(row)
+        #     return array
+        # return pascal(numRows)
                 
-#         answer = [[1]]
+        answer = [[1]]
         
-#         for i in range(numRows-1):
-#             temp = [0] + answer[-1] + [0]
-#             temp2 = []
-#             for j in range(len(temp) - 1):
-#                 temp2.append(temp[j]+temp[j+1])
-#             answer.append(temp2)
+        for i in range(numRows-1):
+            temp = [0] + answer[-1] + [0]
+            temp2 = []
+            for j in range(len(temp) - 1):
+                temp2.append(temp[j]+temp[j+1])
+            answer.append(temp2)
             
-#         return answer
+        return answer
 

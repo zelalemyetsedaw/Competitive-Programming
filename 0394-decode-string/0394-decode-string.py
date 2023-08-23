77af3d530f243b320b9f03f51b696fa4fdc48c7d
zelalemyetsedaw <zy94988@gmail.com>
2023-08-23 15:35:37 +0300

    Time: 44 ms (41.94%), Space: 16.4 MB (38.83%) - LeetHub

--- 0394-decode-string/0394-decode-string.py
+++ 0394-decode-string/0394-decode-string.py
@@ -1,23 +1,23 @@
 class Solution:
     def decodeString(self, s: str) -> str:
         
-        def recursion(index,lists):
-            if index == len(s):
-                return lists
-            if s[index] == "]":
-                string = ""
-                number = ""
-                while lists and lists[-1] != "[":
-                    string = lists.pop() + string
-                lists.pop()
-                while  lists and lists[-1].isdigit():
-                    number = lists.pop() + number
-                
-                string = int(number) * string
-                lists.append(string)
+        
+        def helper(i,n,l):
+            if i>=len(s):
+                return ""
+
+            if s[i]=="]":
+                l[0] = i #we keep track where we left
+                return ""
+
+            if s[i]=="[":
+                return int(n)*helper(i+1,"",l)+helper(l[0]+1,"",l)
+
+            if s[i].isnumeric():
+                return helper(i+1,n+s[i],l)
+
             else:
-                lists.append(s[index])
-            
-            return recursion(index+1,lists)
-            
-        return "".join(recursion(0,[]))
+                return s[i]+helper(i+1,n,l)
+
+        l = [0]
+        return helper(0,"",l)

c65bdfdfb5d9b14721c23f01e5f2e09dc222ec35
zelalemyetsedaw <zy94988@gmail.com>
2023-08-25 10:39:22 +0300

    Time: 50 ms (41.21%), Space: 16.5 MB (67.57%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,13 +1,16 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        def rec(nums, pair):
-            if len(pair) == len(nums):
-                ans.add(pair)
+        
+        answer = []
+        
+        def permutations(temp, answer):
+            if len(temp) == len(nums):
+                answer.append(temp[:])  # Append a copy of temp
                 return
+                
             for num in nums:
-                if num not in pair:
-                    rec(nums, pair + (num,))
-
-        ans = set()
-        rec(nums, ())
-        return ans
+                if num not in temp:
+                    permutations(temp + [num], answer)  # Use a list instead of a tuple
+                    
+        permutations([], answer)  # Start with an empty list
+        return answer

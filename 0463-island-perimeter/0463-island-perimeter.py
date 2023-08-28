d6347b6298fb1a44266822cc9fe98f2006a633f0
zelalemyetsedaw <zy94988@gmail.com>
2023-08-28 16:35:20 +0300

    Time: 678 ms (10.00%), Space: 24.7 MB (22.87%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -6,19 +6,19 @@ class Solution:
         directions = [(0,1),(1,0),(0,-1),(-1,0)]
         visited = [[False] * len(grid[0]) for _ in range(len(grid))]
         
-        
-        def dfs(row,col):
-            if not inbound(row,col) or grid[row][col] == 0  :
-                return 1
-            if visited[row][col]:
-                return 0
+        def dfs(row,col,count):
             visited[row][col] = True
-            count = 0
             
             for rowchange,colchange in directions:
                 newrow = row + rowchange
                 newcol = col + colchange
-                count += dfs(newrow,newcol)
+                
+                if not inbound(newrow,newcol) or grid[newrow][newcol] == 0:
+                    count += 1
+                
+                if inbound(newrow,newcol) and not visited[newrow][newcol] and grid[newrow][newcol] == 1:
+                    
+                    count = dfs(newrow,newcol,count)
                 
                 
             return count 
@@ -26,5 +26,4 @@ class Solution:
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    return dfs(i,j)
-                 
+                    return dfs(i,j,0)

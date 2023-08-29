649f776e711e20231cfa23a9d595c3349272bf03
zelalemyetsedaw <zy94988@gmail.com>
2023-08-29 21:29:29 +0300

    Time: 61 ms (55.23%), Space: 16.2 MB (86.63%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.py
+++ 0994-rotting-oranges/0994-rotting-oranges.py
@@ -8,11 +8,14 @@ class Solution:
         
         visited = set()
         queue = deque()
+        ones = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == 2:
                     queue.append((i,j))
                     visited.add((i,j))
+                elif grid[i][j] == 1:
+                    ones += 1
         
         second = 0
         directions = [(0,1),(1,0),(0,-1),(-1,0)]
@@ -26,15 +29,14 @@ class Solution:
                 
                 for a,b in directions:
                     newdx,newdy = dx+a,dy+b
-                    if inbound(newdx,newdy) and (newdx,newdy) not in visited and grid[newdx][newdy] != 0:
+                    if inbound(newdx,newdy) and (newdx,newdy) not in visited and grid[newdx][newdy] == 1:
                         queue.append((newdx,newdy))
                         visited.add((newdx,newdy))
                         grid[newdx][newdy] = 2
+                        ones -= 1
                         
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    return -1
+        if ones > 0:
+            return -1
         return second - 1 if second > 0 else 0
                     
                 

64f93a1af727d41e5eeec88fb9dd92a56bc9d816
zelalemyetsedaw <zy94988@gmail.com>
2023-08-30 13:48:13 +0300

    Time: 212 ms (67.72%), Space: 17.2 MB (61.96%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,45 +1,34 @@
 class Solution:
-    
-    class UnionFind:
-        def __init__(self, n: int):
-            self.parent = [i for i in range(n)]
-            self.treeSize = [1 for _ in range(n)]
-            self.count = n
-        
-        def union(self, a: int, b: int):
-            a = self._find(a)
-            b = self._find(b)
-            if a == b:
-                return
-            if self.treeSize[a] < self.treeSize[b]:
-                self.parent[a] = b
-                self.treeSize[b] += self.treeSize[a]
-            else:
-                self.parent[b] = a
-                self.treeSize[a] += self.treeSize[b]
-            self.count -= 1
-    
-        def _find(self, x: int) -> int:
-            while self.parent[x] != x:
-                self.parent[x] = self.parent[self.parent[x]]
-                x = self.parent[x]
-            return x
-			
-
-        def isConnected(self, a: int, b: int) -> bool:
-            a = self._find(a)
-            b = self._find(b)
-            return a == b
-    
-    
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        if not isConnected or len(isConnected) == 0 or len(isConnected[0]) == 0:
-            return 0
-        lenY, lenX = len(isConnected), len(isConnected[0])
-        uf = self.UnionFind(lenY)
-        cities = lenY
-        for y in range(lenY):
-            for x in range(y + 1, lenX):
-                if isConnected[y][x] == 1:
-                    uf.union(y, x)
-        return uf.count
+        
+        graph = defaultdict(list)
+        def dfs(stack):
+            while stack:
+                child = stack.pop()
+                for ch in graph[child]:
+                    if ch not in visited:
+                        stack.append(ch)
+                        visited.add(ch)
+        
+        n = len(isConnected)
+        
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] == 1:
+                    graph[i].append(j)
+                    
+                    
+        visited = set()
+        stack = []
+        count = 0
+        
+        for i in graph:
+            if i not in visited:
+                stack.append(i)
+                dfs(stack)
+                count += 1
+        
+                        
+        return count
+                
+                

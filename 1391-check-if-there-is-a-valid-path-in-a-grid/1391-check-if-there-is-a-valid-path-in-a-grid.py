f234f805438ad364be994f13199fee6dc443ce4b
zelalemyetsedaw <zy94988@gmail.com>
2023-08-30 21:35:21 +0300

    Time: 1193 ms (85.49%), Space: 31.5 MB (76.68%) - LeetHub

--- 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
+++ 1391-check-if-there-is-a-valid-path-in-a-grid/1391-check-if-there-is-a-valid-path-in-a-grid.py
@@ -1,36 +1,40 @@
 class Solution:
     def hasValidPath(self, grid: List[List[int]]) -> bool:
-        destination = (len(grid) - 1, len(grid[0]) - 1)
+        
+        m = len(grid)
+        n = len(grid[0])
+        
+        def inbound(r,c):
+            return 0 <= r < m and 0 <= c < n
+        
         directions = {
-            1: [(0, -1), (0, 1)],
-            2: [(-1, 0), (1, 0)],
-            3: [(0, -1), (1, 0)],
-            4: [(0, 1), (1, 0)],
-            5: [(0, -1), (-1, 0)],
-            6: [(0, 1), (-1, 0)]
+            1:[(0,-1),(0,1)],
+            2:[(1,0),(-1,0)],
+            3:[(0,-1),(1,0)],
+            4:[(0,1),(1,0)],
+            5:[(-1,0),(0,-1)],
+            6:[(-1,0),(0,1)]
         }
-
-        def inbound(row, col):
-            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
-
-        visited = set([(0, 0)])
-
-        def dfs(row, col):
-            if (row, col) == destination:
-                return True
+        
+        stack = [(0,0)]
+        destination = (m-1,n-1)
+        visited = set([(0,0)])
+        flag = False
+        while stack:
+            r,c = stack.pop()
+            if (r,c) == destination:
+                flag = True
             
-            for row_change, col_change in directions[grid[row][col]]:
-                new_row = row + row_change
-                new_col = col + col_change
+            
+            for dr,dc in directions[grid[r][c]]:
+                newrow , newcol = r + dr, c + dc
                 
-                if (inbound(new_row, new_col) and
-                    (new_row, new_col) not in visited and
-                    (-row_change, -col_change) in directions[grid[new_row][new_col]]):
-                    visited.add((new_row, new_col))
-                    found = dfs(new_row, new_col)
-                    if found:
-                        return True
-            
-            return False
-
-        return dfs(0, 0)
+                
+                if inbound(newrow,newcol) and (newrow,newcol) not in visited and (-dr,-dc) in directions[grid[newrow][newcol]]:
+                    stack.append((newrow,newcol))
+                    visited.add((newrow,newcol))
+                    
+                    
+        return flag
+                        
+                

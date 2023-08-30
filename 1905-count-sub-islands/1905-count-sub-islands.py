8b8e3f79dbe944ebbbb5869fa32333fee745ce87
zelalemyetsedaw <zy94988@gmail.com>
2023-08-30 14:31:42 +0300

    Time: 2631 ms (69.53%), Space: 40.5 MB (91.02%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.py
+++ 1905-count-sub-islands/1905-count-sub-islands.py
@@ -1,28 +1,43 @@
 class Solution:
     def countSubIslands(self, grid1: List[List[int]], grid2: List[List[int]]) -> int:
         
+        m = len(grid1)
+        n = len(grid1[0])
         
+        def inbound(r,c):
+            return 0 <= r < m and 0 <= c < n
+        
+        directions = [(1,0),(0,1),(-1,0),(0,-1)]
         visited = set()
-        def  dfs(l,r):
-            if l<0 or r<0 or l==len(grid1) or r== len(grid1[0]) or (l,r) in visited or grid2[l][r] == 0:
-                return True
-            
-            res = True
-            if grid1[l][r] == 0:
-                res = False
-            
-            visited.add((l,r))
-            res = dfs(l+1,r) and res
-            res = dfs(l,r+1) and res
-            res = dfs(l-1,r) and res
-            res = dfs(l,r-1) and res
-            
-            return res
+        def dfs(i,j):
+            stack = []
+            stack.append((i,j))
+            visited.add((i,j))
+            countgrid1 , countgrid2 = 0,0
+            while stack:
+                r,c = stack.pop()
+                if grid1[r][c] == 1:
+                        countgrid1 += 1
+                countgrid2 += 1
+
+
+                for dr,dc in directions:
+                    newrow,newcol = r + dr,c + dc
+                    if inbound(newrow,newcol) and grid2[newrow][newcol] != 0 and (newrow,newcol) not in visited:
+                        stack.append((newrow,newcol))
+                        visited.add((newrow,newcol))
+            return countgrid2 == countgrid1
         
-        count = 0
-        for i in range(len(grid2)):
-            for j in range(len(grid2[0])):
-                if grid2[i][j] and (i,j) not in visited and dfs(i,j):
-                    count += 1
+        answer = 0
+        for i in range(m):
+            for j in range(n):
+                if grid2[i][j] == 1 and (i,j) not in visited and dfs(i,j):
+                    answer += 1
+        
+        return answer
+        
                 
-        return count
+                
+            
+        
+        

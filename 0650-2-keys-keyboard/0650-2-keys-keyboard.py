6ca3b0079826dd646b636f2a3827511ca346a9c2
zelalemyetsedaw <zy94988@gmail.com>
2023-09-25 21:20:59 +0300

    Time: 922 ms (5.98%), Space: 17.2 MB (39.23%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,14 +1,19 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        factorization = []
-        d = 2
-
-        while d * d <= n:
-            while n % d == 0:
-                factorization.append(d)
-                n //= d
-            d += 1
-        if n > 1:
-            factorization.append(n)
-
-        return sum(factorization)
+        
+        def recursion(copy_length,word_length,n):
+            if word_length == n:
+                return 0
+            
+            copy = float("inf")
+            paste = float("inf")
+            
+            if copy_length != word_length:
+                copy = 1 + recursion(word_length,word_length,n)
+            if copy_length != 0 and word_length + copy_length <= n:
+                paste = 1 + recursion(copy_length,word_length+copy_length,n)
+                
+            return min(copy,paste)
+        
+        return recursion(0,1,n)
+        

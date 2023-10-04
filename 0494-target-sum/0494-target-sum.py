2bbd4dd2d689a911a108bd588a7d6d24af86f627
zelalemyetsedaw <zy94988@gmail.com>
2023-10-04 07:12:29 +0300

    Time: 257 ms (57.13%), Space: 33.8 MB (54.13%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,29 +1,20 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-                
-        memo = {}
         
-        def recursion(cursum,index):
-       
-            
-            if index == len(nums):
-                if cursum == target:
-                    return 1
+        memo = defaultdict(int)
+        def recursion(summ,index):
+            if (summ,index) in memo:
+                return memo[(summ,index)]
+            if summ == 0 and index == 0:
+                return 1
+            elif index == 0:
                 return 0
             
-            if (index,cursum) in memo:
-                return memo[(index,cursum)]
             
-            
-            
-            
-            memo[(index,cursum)] = recursion(cursum + nums[index],index + 1) +                                                  recursion(cursum - nums[index] ,index+1)
-            
-            
-            
-            
-            return memo[(index,cursum)]
-            
-       
-        return recursion(0,0)
+            memo[(summ,index)] = recursion(summ-nums[index-1],index-1) + recursion(summ+nums[index-1],index - 1)
+            return memo[(summ,index)]
+        
+        return recursion(target,len(nums))
+                
+
              

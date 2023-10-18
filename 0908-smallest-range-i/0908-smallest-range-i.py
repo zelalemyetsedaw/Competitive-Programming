5522d9dbeaff01590db4d07935873e55e4db1c9e
zelalemyetsedaw <zy94988@gmail.com>
2023-10-18 09:55:50 +0300

    Time: 111 ms (42.21%), Space: 17.6 MB (66.09%) - LeetHub

---
+++ 0908-smallest-range-i/0908-smallest-range-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def smallestRangeI(self, nums: List[int], k: int) -> int:
+        
+        M, m = max(nums), min(nums)
+        diff, extension = M - m, 2*k
+        
+        if diff <= extension:
+            return 0
+        
+        else:
+            return diff - extension
